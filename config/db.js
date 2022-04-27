362693b3e9113a2d2a2d112a1114ed2b2935466b
SHIKI65536 <shiki65536@gmail.com>
2022-04-27 11:10:32 +0800

    add backend

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongooseURI');
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect(db);
+
+        console.log(('Mongodb Connected'));
+    }catch(err){
+        console.log(err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+// const connectDB = require('./config/db')
+// const path = require('path')
+
+const app = express()
+
+// connectDB();
+
+// app.use(express.json({extended: false}));
+
+app.get('/', (req, res) => res.send('API running'));
+
+// define routes
+// app.use('/users', require('./routes/users'));
+// app.use('/auth', require('./routes/auth'));
+// app.use('/post', require('./routes/post'));
+
+//serve static assets
+// if(process.env.NODE_ENV === 'production') {
+//     //set static folder
+//     app.use(express.static('client/build'))
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
+
+const PORT = process.env.PORT || 5001
+
+app.listen(PORT, ()=> console.log(`sever started on  PORT ${PORT}`))
